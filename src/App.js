436c0a07b05b3a533462b53c0bd0073c3e018d41
Joaquin Linares <joacolinares2003@gmail.com>
2024-09-02 18:31:03 -0500

    Cambios V2

--- src/App.js
+++ src/App.js
@@ -8,16 +8,14 @@ import { Polygon } from "@thirdweb-dev/chains";
 
 export default function Home() {
   const wallet = useAddress();
-  const [balance, setBalance] = useState();
-  const [currentNFT, setCurrentNFT] = useState(69);
-  const inputNFTs = useRef(0);
-  const [quemados, setQuemados] = useState(0);
-  const [balanceToken, setBalanceToken] = useState(0);
-  const [aprobado, setAprobado] = useState(false);
-  const [aprobado2, setAprobado2] = useState(false);
-  const [totalMinteado, settotalMinteado] = useState(0);
-  const [coLibre, setCoLibre] = useState(0);
   const [referral, setReferral] = useState("");
+  const [referralOfUser, setReferralOfUser] = useState("");
+  const [directReferrals, setDirectReferrals] = useState("");
+  const [missedOpportunities, setMissedOpportunities] = useState("");
+  const [payedExtra, setPayedExtra] = useState("");
+  const [totalTree, setTotalTree] = useState("");
+  const [balanceContrato, setBalanceContrato] = useState("");
+  const [balanceVault, setBalanceVault] = useState("");
   const [infoPools, setInfoPools] = useState({
     data: [
       { pool: 1, usersCount: 0, wallets: [] },
@@ -34,7 +32,8 @@ export default function Home() {
   const loadInfo = async () => {
     console.log(wallet);
     const sdk = new ThirdwebSDK("polygon");
-    const contract = await sdk.getContract("0x7b17217290caa5fC4c5942eE253b079d2A53b95B", abi);
+    const contract = await sdk.getContract("0x6619B3D7bCcb53F039C9905E190158e694E4A2B4", abi);
+    const contractToken = await sdk.getContract("0x31B4245d9f88DA6Fa01A14398adA46b177c7F2ba", abiToken);
 
     if (wallet != undefined) {
       console.log(wallet);
@@ -47,6 +46,23 @@ export default function Home() {
           return { pool: poolId, usersCount: usersCount, wallets: getQueue };
         })
       );
+      const users = await contract.call("users", [wallet]);
+      console.log(users)
+      if(users.referrer == "0x0000000000000000000000000000000000000000"){
+        setReferralOfUser("USTED NO TIENE REFERIDO RECUERDE EN SU PRIMERA COMPRA PONERLO ARRIBA")
+      }else{
+        setReferralOfUser(users.referrer)
+      }
+      setDirectReferrals(parseInt(users.directReferrals._hex,16))
+      setMissedOpportunities(parseInt(users.missedOpportunities._hex,16))
+      setPayedExtra(parseInt(users.payedExtra._hex,16))
+      setTotalTree(parseInt(users.totalTree._hex,16))
+      
+      const balanceContrato = await contractToken.call("balanceOf", ["0x6619B3D7bCcb53F039C9905E190158e694E4A2B4"]);
+      const balanceVault = await contractToken.call("balanceOf", ["0x3a74C3d3c9F197A4E1f8DEC55031C8f5931F9FFe"]);
+
+      setBalanceContrato(parseInt(balanceContrato._hex,16))
+      setBalanceVault(parseInt(balanceVault._hex,16))
 
       setInfoPools({ data: updatedData, status: true });
     }
@@ -70,9 +86,9 @@ export default function Home() {
     const sdk = ThirdwebSDK.fromSigner(signer, Polygon);
 
     const contractToken = await sdk.getContract("0x31B4245d9f88DA6Fa01A14398adA46b177c7F2ba", abiToken);
-    await contractToken.call("approve", ["0x7b17217290caa5fC4c5942eE253b079d2A53b95B", ethers.constants.MaxUint256]);
+    await contractToken.call("approve", ["0x6619B3D7bCcb53F039C9905E190158e694E4A2B4", ethers.constants.MaxUint256]);
 
-    const contract = await sdk.getContract("0x7b17217290caa5fC4c5942eE253b079d2A53b95B", abi);
+    const contract = await sdk.getContract("0x6619B3D7bCcb53F039C9905E190158e694E4A2B4", abi);
 
     if (referral != "") {
       await contract.call("joinPool", [pool, referral, wallet]);
@@ -101,6 +117,28 @@ export default function Home() {
                   value={referral}
                   onChange={(e) => setReferral(e.target.value)}
                 />
+                <br />
+                <br />
+                <p>INFO GENERAL</p>
+                <p>Balance Contrato: {balanceContrato / 1000000}</p>
+                <br />
+                <p>Balance Vault: {balanceVault / 1000000}</p>
+                <br />
+                <br />
+                <br />
+
+                <p>INFO PERSONAL</p>
+                <p>Wallet conectada: {wallet}</p>
+                <br />
+                <p>Sponsor: {referralOfUser}</p>
+                <br />
+                <p>Cantidad de directos: {directReferrals}</p>
+                <br />
+                <p>Total perdido: {missedOpportunities / 1000000}$</p>
+                <br />
+                <p>Cantidad de dinero en excedente: {payedExtra / 1000000}$</p> 
+                <br />
+                <p>Cantidad generado por mi arbol: {totalTree / 1000000}$</p> 
                 <br />
                 <br />
                 {infoPools.data.map((poolInfo, index) => (
